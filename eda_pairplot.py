0c606502cc60f6ef5948da03a9e8aa676d997b32
Michaël Schuh <michael.schuh.34@gmail.com>
2022-07-28 19:58:36 +0200

    add epa pairplot

---
+++ eda_pairplot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train = pd.read_csv('./data/train.csv')
+#test = pd.read_csv('./data/test.csv')
+
+sns.set(style="ticks", color_codes=True)
+g = sns.pairplot(train, kind="reg", plot_kws={'line_kws':{'color':'red'}})
+plt.savefig('eda/pairplot_train.png')
