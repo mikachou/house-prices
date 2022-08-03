6a9ccf8648d23f3bc725509f52bfcafda19d42c5
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-03 21:36:28 +0200

    random forest

---
+++ inc/model/rndforest.py
@@ -0,0 +1,19 @@
+"""Provide decision tree regression
+"""
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from optuna.distributions import \
+    CategoricalDistribution, IntUniformDistribution, UniformDistribution
+
+params = {
+    'n_estimators': IntUniformDistribution(100, 500),
+    'max_depth': IntUniformDistribution(10, 100),
+    'min_samples_split': IntUniformDistribution(3, 300),
+    'min_samples_leaf': IntUniformDistribution(3, 300),
+    'max_features': CategoricalDistribution(['sqrt']),
+    'max_features': UniformDistribution(0.1, 1.0),
+    'max_leaf_nodes': IntUniformDistribution(5, 300),
+    'max_samples': UniformDistribution(0.1, 1.0),
+}
+
+model = RandomForestRegressor()
