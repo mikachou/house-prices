18bc73d360d31b9f8f2941ea674bb93ad91f044f
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-04 23:58:40 +0200

    lightgbm regressor model

---
+++ inc/model/lightgbm.py
@@ -0,0 +1,18 @@
+"""Provide lightgbm regression
+"""
+from lightgbm import LGBMRegressor
+from optuna.distributions import \
+    CategoricalDistribution, IntUniformDistribution, \
+    UniformDistribution, LogUniformDistribution
+
+params = {
+    'n_estimators': IntUniformDistribution(50, 500),
+    'max_depth': IntUniformDistribution(3, 300),
+    'num_leaves': IntUniformDistribution(3, 1500),
+    'learning_rate': LogUniformDistribution(1e-4, 1e-1),
+    'subsample': UniformDistribution(0.1, 1.0),
+    'colsample_bytree': UniformDistribution(0.1, 1.0),
+    'colsample_bynode': UniformDistribution(0.1, 1.0),
+}
+
+model = LGBMRegressor()
