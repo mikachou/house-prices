f44bd502f51bd13fe5b9affeb9e2c548fcb97261
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-05 16:46:08 +0200

    log xgboost model

---
+++ inc/model/log_xgboost.py
@@ -0,0 +1,22 @@
+"""Provide catboost regression
+"""
+import numpy as np
+from xgboost import XGBRegressor
+from sklearn.compose import TransformedTargetRegressor
+from optuna.distributions import \
+    CategoricalDistribution, IntUniformDistribution, \
+    UniformDistribution, LogUniformDistribution
+
+params = {
+    'regressor__n_estimators': IntUniformDistribution(50, 500),
+    'regressor__max_depth': IntUniformDistribution(3, 300),
+    'regressor__max_leaves': IntUniformDistribution(3, 1500),
+    'regressor__learning_rate': LogUniformDistribution(1e-4, 1e-1),
+    'regressor__subsample': UniformDistribution(0.1, 1.0),
+    'regressor__colsample_bytree': UniformDistribution(0.1, 1.0),
+    'regressor__colsample_bylevel': UniformDistribution(0.1, 1.0),
+    'regressor__colsample_bynode': UniformDistribution(0.1, 1.0),
+}
+
+model = TransformedTargetRegressor(
+    regressor=XGBRegressor(), func=np.log1p, inverse_func=np.expm1)
