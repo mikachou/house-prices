4d07cf2dcad54de7c45921cdab7fd5b83aa63825
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-06 20:29:48 +0200

    catboost model

--- inc/model/catboost.py
+++ inc/model/catboost.py
@@ -6,10 +6,13 @@ from optuna.distributions import \
     UniformDistribution, LogUniformDistribution
 
 params = {
-    'iterations': IntUniformDistribution(100, 200),
-    'learning_rate': LogUniformDistribution(0.03, 0.1),
-    'depth': IntUniformDistribution(2, 8),
-    'l2_leaf_reg': LogUniformDistribution(0.2, 3)
+    'n_estimators': IntUniformDistribution(100, 1000),
+    'learning_rate': LogUniformDistribution(1e-3, 1e-1),
+    'l2_leaf_reg': LogUniformDistribution(1e-4, 1e-1),
+    'colsample_bylevel': UniformDistribution(0.1, 1.0),
+    'subsample': UniformDistribution(0.1, 1.0),
+    'max_depth': IntUniformDistribution(1, 8),
+    'min_data_in_leaf': IntUniformDistribution(1, 300),
 }
 
-model = CatBoostRegressor()
+model = CatBoostRegressor(silent=True, thread_count=1)
