b6990d1c6ab244fc6105baa62d6f06ed69d9d724
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-07 13:27:25 +0200

    transformations on svm target

--- inc/model/bc_svm.py
+++ inc/model/bc_svm.py
@@ -1,6 +1,7 @@
-"""Provide log svm regression
+"""Provide box-cox svm regression
 """
-import numpy as np
+import pandas as pd
+from sklearn.preprocessing import PowerTransformer
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.svm import SVR
 from optuna.distributions import \
@@ -8,9 +9,14 @@ from optuna.distributions import \
 # import warnings
 # warnings.filterwarnings('ignore') 
 
+train = pd.read_csv('./data/train.csv')
+
+tr = PowerTransformer(method='box-cox')
+tr.fit(train['SalePrice'].values.reshape(-1, 1))
+
 params = {
     'regressor__C': LogUniformDistribution(1e-3, 1e3),
 }
 
 model = TransformedTargetRegressor(
-    regressor=SVR(), func=np.log1p, inverse_func=np.expm1)
+    regressor=SVR(), func=tr.transform, inverse_func=tr.inverse_transform)
