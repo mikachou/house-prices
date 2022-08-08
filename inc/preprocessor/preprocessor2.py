e6e67be4b49c5a8b2504e45d6b50c837e50be177
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-08 16:47:37 +0200

    preprocessor refactoring, 2 preprocessors

--- inc/preprocessor/preprocessor2.py
+++ inc/preprocessor/preprocessor2.py
@@ -1,9 +1,9 @@
 from inc.preprocessor.common.preprocessor import common_preprocessor
-from sklearn.impute import KNNImputer
+from sklearn.impute import SimpleImputer
 
 def preprocessor(train, test):
     pre = common_preprocessor(train, test)
 
-    pre.steps.append(('impute', KNNImputer()))
+    pre.steps.append(('impute', SimpleImputer(strategy='mean')))
 
     return pre
