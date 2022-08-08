2e3cf367e8d649fa91c8df9a25e6340258bb3cb4
Michaël Schuh <michael.schuh.34@gmail.com>
2022-08-08 16:23:16 +0200

    random forest improvements

--- inc/model/rndforest.py
+++ inc/model/rndforest.py
@@ -6,12 +6,12 @@ from optuna.distributions import \
     CategoricalDistribution, IntUniformDistribution, UniformDistribution
 
 params = {
-    'n_estimators': IntUniformDistribution(100, 500),
-    'max_depth': IntUniformDistribution(10, 100),
+    'n_estimators': IntUniformDistribution(100, 1000),
+    'max_depth': IntUniformDistribution(10, 1000),
     'min_samples_split': IntUniformDistribution(3, 300),
     'min_samples_leaf': IntUniformDistribution(3, 300),
     'max_features': UniformDistribution(0.1, 1.0),
-    'max_leaf_nodes': IntUniformDistribution(5, 300),
+    'max_leaf_nodes': IntUniformDistribution(5, 1000),
     'max_samples': UniformDistribution(0.1, 1.0),
 }
 
